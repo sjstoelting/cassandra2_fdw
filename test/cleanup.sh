f2d342e9c82222f062ff0258a099f04bb255420d
Stefanie Janine Stölting <stefanie@ProOpenSource.eu>
2024-10-15 09:30:40 +0200

    Cleanup does not return error messages when a container or the network are not existing while they are removed.

--- test/cleanup.sh
+++ test/cleanup.sh
@@ -20,30 +20,32 @@ do
 
     # Deleting the cassandra container
     echo "Deleting container $CASSANDRA_CONTAINER ..."
-    sudo docker stop $CASSANDRA_CONTAINER
-    sudo docker rm $CASSANDRA_CONTAINER
+    sudo docker stop $CASSANDRA_CONTAINER 2> /dev/null || true
+    sudo docker rm $CASSANDRA_CONTAINER 2> /dev/null || true
 done
 
 ## PostgreSQL
 for PG_VERSION in "${POSTGRESQL_VERSIONS[@]}"
 do
     # Set container names and PostgreSQL images
-    POSTGRESQL_CONTAINER="PostgreSQL_${PG_VERSION}_N"
+    POSTGRESQL_CONTAINER="POSTGRESQL_${PG_VERSION}_N"
 
     echo "Deleting container $POSTGRESQL_CONTAINER ..."
-    sudo docker stop $POSTGRESQL_CONTAINER
-    sudo docker rm $POSTGRESQL_CONTAINER
+    sudo docker stop $POSTGRESQL_CONTAINER 2> /dev/null || true
+    sudo docker rm $POSTGRESQL_CONTAINER 2> /dev/null || true
 done
 
 # PostgreSQL result server
 if [ $REMOVE_RESULT_SERVER -eq 1 ]; then
-    POSTGRESQL_RESULT_CONTAINER="PostgreSQL_RESULT_${PostgreSQL_RESULT_VERSION}_N"
-    sudo docker stop $POSTGRESQL_RESULT_CONTAINER
-    sudo docker rm $POSTGRESQL_RESULT_CONTAINER
+    POSTGRESQL_RESULT_CONTAINER="POSTGRESQL_RESULT_${POSTGRESQL_RESULT_VERSION}_N"
+    sudo docker stop $POSTGRESQL_RESULT_CONTAINER 2> /dev/null || true
+    sudo docker rm $POSTGRESQL_RESULT_CONTAINER 2> /dev/null || true
 
     # Remove the network
     echo "Removing network $DOCKER_NETWORK ..."
-    sudo docker network rm $DOCKER_NETWORK
+    sudo docker network rm $DOCKER_NETWORK 2> /dev/null || true
+
+    echo "Result container $POSTGRESQL_RESULT_CONTAINER and docker network $DOCKER_NETWORK have been removed"
 else
     echo "Result container $POSTGRESQL_RESULT_CONTAINER and docker network $DOCKER_NETWORK are still up!"
 fi
